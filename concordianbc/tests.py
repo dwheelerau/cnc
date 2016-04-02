d008c74e382e7d11fb99fb850a50495f2768026e
Dave Wheeler <dwheelerau@gmail.com>
2016-04-02 15:44:52 +1300

    add home results about links and tests pass

--- concordianbc/tests.py
+++ concordianbc/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from concordianbc.views import home_page
+from django.http import HttpRequest
+# from django.template.loader import render_to_string
 
 
 # Create your tests here.
@@ -9,3 +11,22 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_url(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(
+            '<title>Welcome to Concordia netball!</title>',
+            response.content.decode())
+        self.assertTrue(response.content.strip().endswith(b'</html>'))
+
+    def test_home_page_has_three_navigaton_links(self):
+        # test home, results, about links
+        # <a href>
+        request = HttpRequest()
+        response = home_page(request)
+        # home_page_string = render_to_string('home.html')
+        self.assertIn('<a href="">Home</a>', response.content.decode())
+        self.assertIn('<a href="">Results</a>', response.content.decode())
+        self.assertIn('<a href="">About</a>', response.content.decode())
--- functional_tests/test.py
+++ functional_tests/test.py
@@ -16,5 +16,36 @@ class NewVisitorTest(LiveServerTestCase):
     def test_user_hits_homepage(self):
         # a new user types in the url and finds the homepage
         self.browser.get(self.live_server_url)
-        title = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('Welcome to Concordia netball!', title)
+        # title = self.browser.find_element_by_tag_name('h1').text
+        # self.assertIn('Welcome to Concordia netball!', title)
+        self.assertIn('Welcome', self.browser.title)
+
+    def test_user_can_navigate_site(self):
+        # the visitor sees two links at the top of the page
+        # these are: home, results, about
+        self.browser.get(self.live_server_url)
+        self.browser.implicitly_wait(5)
+
+        home_link = self.browser.find_element_by_link_text('Home')
+        self.assertIn('Home', home_link.text)
+
+        results_link = self.browser.find_element_by_link_text('Results')
+        self.assertIn('Results', results_link.text)
+
+        about_link = self.browser.find_element_by_link_text('About')
+        self.assertIn('About', about_link.text)
+
+        # about_link.click()
+        # print(self.live_server_url + '/results/')
+        # self.browser.get(self.live_server_url + '/results/')
+        # self.assertIn('Results', self.browser.title)
+
+
+        self.browser.implicitly_wait(5)
+
+        # she clicks on results and gets taken onto a new page
+        # the results page also has these three links
+        # so she now clicks on the about
+        # she gets taken to a new page
+        # this new page also has the three links
+        self.fail('Finish the tests')
